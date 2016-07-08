20386c5186afaf79cf348a3ca8fbdfe4d5fc708b
gauravkhuperkar <gauravkh@thoughtworks.com>
2016-07-09 00:33:18 +0530

    written logic of redirecting the files in server.js

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res){
-	res.sendFile(__dirname + '/index.html');
+	console.log(req.url)
+	res.redirect('/index.html');
 })
+
+app.use(express.static('.'));
 
 app.listen(4000,function(){
 	console.log('server is listing at',4000);
